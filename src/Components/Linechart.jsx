186472a7227687c92281dcd710ead00f66fb833b
Dhivyabharathi K <dhivyabharathi.k@jmangroup.com>
2023-09-25 17:19:35 +0530

    currency vice-versa added

--- src/Components/Linechart.jsx
+++ src/Components/Linechart.jsx
@@ -1,48 +1,102 @@
-import React, { useRef, useEffect } from 'react';
-import Chart from 'chart.js/auto';  // Correct import statement
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+import Chart from 'chart.js/auto';
 
-const LineChart = ({ historicalExchangeRates }) => {
+const ACCESS_KEY = '80b1600a98127f7a295e319ee678e4a4'; // Your Fixer API access key
+const BASE_API_URL = 'http://data.fixer.io/api';
+
+const ExchangeRateChart = ({ selectedDate }) => {
+  const [exchangeRates, setExchangeRates] = useState(null);
   const chartRef = useRef(null);
 
   useEffect(() => {
-    if (!chartRef.current || !historicalExchangeRates) return;
+    const fetchExchangeRates = async () => {
+      try {
+        const historicalEndpoint = `${BASE_API_URL}/${selectedDate}?access_key=${ACCESS_KEY}`;
+        const response = await axios.get(historicalEndpoint);
+        setExchangeRates(response.data.rates);
+      } catch (error) {
+        console.error('Error fetching exchange rates:', error);
+      }
+    };
 
-    const labels = Object.keys(historicalExchangeRates);
-    const data = Object.values(historicalExchangeRates);
+    fetchExchangeRates();
+  }, [selectedDate]);
 
-    const ctx = chartRef.current.getContext('2d');
+  useEffect(() => {
+    if (exchangeRates) {
+      const currencies = Object.keys(exchangeRates);
+      const data = Object.values(exchangeRates);
 
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels,
-        datasets: [
-          {
-            label: 'Exchange Rate',
-            data,
-            fill: false,
-            borderColor: 'rgba(75,192,192,1)',
-            borderWidth: 2,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          x: {
-            type: 'time',
-            time: {
-              unit: 'month',
+      if (chartRef.current) {
+        chartRef.current.destroy();
+      }
+
+      const ctx = document.getElementById('exchangeRateChart').getContext('2d');
+
+      chartRef.current = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: currencies, // Use currencies as labels on the X-axis
+          datasets: [
+            {
+              label: `Exchange Rate Change from ${selectedDate}`,
+              data: data,
+              borderColor: getBorderColor(currencies.length),
+              borderWidth: 2,
+              pointRadius: 5,
+              pointBackgroundColor: 'rgba(75, 192, 192, 1)',
+            },
+          ],
+        },
+        options: {
+          scales: {
+            x: {
+              display: true,
+              title: {
+                display: true,
+                text: 'Currency',
+              },
+            },
+            y: {
+              display: true,
+              title: {
+                display: true,
+                text: 'Exchange Rate',
+              },
+              beginAtZero: true,
             },
           },
-          y: {
-            beginAtZero: true,
+          plugins: {
+            tooltip: {
+              callbacks: {
+                label: (context) => {
+                  const currency = currencies[context.dataIndex];
+                  const rate = data[context.dataIndex];
+                  return `${currency}: ${rate.toFixed(2)}`;
+                },
+              },
+            },
           },
         },
-      },
-    });
-  }, [historicalExchangeRates]);
+      });
+    }
+  }, [exchangeRates, selectedDate]);
 
-  return <canvas ref={chartRef} />;
+  const getBorderColor = (length) => {
+    const colors = [];
+    for (let i = 0; i < length; i++) {
+      colors.push(`rgba(${i * 30}, ${i * 50}, ${i * 10}, 1)`);
+    }
+    return colors;
+  };
+
+  return (
+    <div>
+      <h2>Exchange Rate Chart</h2>
+      <canvas id="exchangeRateChart" width="400" height="200" />
+    </div>
+  );
 };
 
-export default LineChart;
+export default ExchangeRateChart;
--- src/Pages/CurrencyConverterPage.jsx
+++ src/Pages/CurrencyConverterPage.jsx
@@ -1,51 +1,48 @@
-
-
-import React, { useEffect, useState } from 'react'
-import CurrencyOptions from '../Components/CurrencyOptions'
+import React, { useEffect, useState } from 'react';
+import CurrencyOptions from '../Components/CurrencyOptions';
 import { fetchCurrencies, fetchExchangeRate } from '../Services/Api';
-import '../Styles/CurrencyConverterPage.css'
+import '../Styles/CurrencyConverterPage.css';
 
 function CurrencyConverter() {
-  const [currencyOptions, setCurrencyOptions] = useState([])
-  const [fromAmount, setFromAmount] = useState(0)
-  const [toAmount, setToAmount] = useState(0)
-  const [fromCurrency, setFromCurrency] = useState('')
-  const [toCurrency, setToCurrency] = useState('')
-  const [currencyNames, setCurrencyNames] = useState({})
-  const [selectedDate, setSelectedDate] = useState('');  // Store the selected date
-
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromAmount, setFromAmount] = useState(0);
+  const [toAmount, setToAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState('');
+  const [toCurrency, setToCurrency] = useState('');
+  const [currencyNames, setCurrencyNames] = useState({});
+  const [selectedDate, setSelectedDate] = useState(''); // Store the selected date
 
   const updateFromCurrency = (value) => {
     setFromCurrency(value);
-
   };
+
   const updateToCurrency = (value) => {
     setToCurrency(value);
-
   };
 
   const handleDateChange = (event) => {
     setSelectedDate(event.target.value);
   };
 
-
-
+  const handleSwapCurrencies = () => {
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
+  };
 
   useEffect(() => {
-
     fetchCurrencies()
       .then((res) => res.json())
       .then((data) => {
-        setCurrencyOptions(Object.keys(data.symbols));
-        setFromCurrency(Object.keys(data.symbols)[0]);
-        setToCurrency(Object.keys(data.symbols)[0]);
+        const currencyKeys = Object.keys(data.symbols);
+        setCurrencyOptions(currencyKeys);
+        setFromCurrency(currencyKeys[0]);
+        setToCurrency(currencyKeys[0]); // Make sure we start with different currencies
         setCurrencyNames(data.symbols);
-
       })
       .catch((error) => console.error('Error fetching currencies:', error));
   }, []);
 
-  
+ 
   useEffect(() => {
     if (parseFloat(fromAmount) === 0) {
       setToAmount(0);
@@ -71,20 +68,15 @@ function CurrencyConverter() {
     }
   }, [fromCurrency, toCurrency, fromAmount]);
 
+  useEffect(() => {
+    getHistoricalExchangeRate();
+  }, [fromCurrency, toCurrency, selectedDate]);
+
   const getHistoricalExchangeRate = () => {
     if (selectedDate && fromCurrency && toCurrency) {
       const historicalEndpoint = `http://data.fixer.io/api/${selectedDate}?access_key=80b1600a98127f7a295e319ee678e4a4`;
 
-
-      console.log('Fetching historical exchange rate...');
-      console.log('Selected Date:', selectedDate);
-      console.log('From Currency:', fromCurrency);
-      console.log('To Currency:', toCurrency);
       fetch(historicalEndpoint)
-
-
-
-
         .then((res) => res.json())
         .then((data) => {
           if (data.success) {
@@ -105,35 +97,42 @@ function CurrencyConverter() {
     }
   };
 
-  useEffect(() => {
-    getHistoricalExchangeRate();
-  }, [fromCurrency, toCurrency, selectedDate]);
-  
-
-
-
   return (
     <div className="app">
       <nav className="navbar">
         <h1>Currency Converter</h1>
       </nav>
-      {/* <h1>Currency Converter</h1> */}
       <div className="container">
         <div className="item1">
           <div className="opt-1">
-            <h2>From Currency  </h2>
-            <CurrencyOptions prop="From currency" fromCurrency={fromCurrency} currencyOptions={currencyOptions} updateCurrency={updateFromCurrency} />                    </div>
+            <h2>From Currency</h2>
+            <CurrencyOptions
+              prop="From currency"
+              fromCurrency={fromCurrency}
+              currencyOptions={currencyOptions}
+              updateCurrency={updateFromCurrency}
+            />
+          </div>
+          <div className="swap-button">
+            <button onClick={handleSwapCurrencies}>&#8679;&#8675;&#8681;</button>
+          </div>
           <div className="opt-2">
-            <h2>To Currency  </h2>
-            <CurrencyOptions prop="To currency" toCurrency={toCurrency} currencyOptions={currencyOptions} updateCurrency={updateToCurrency} />                      </div>
+            <h2>To Currency</h2>
+            <CurrencyOptions
+              prop="To currency"
+              toCurrency={toCurrency}
+              currencyOptions={currencyOptions}
+              updateCurrency={updateToCurrency}
+            />
+          </div>
         </div>
         <div className="item2">
-          <h2>Enter Amount in  {fromCurrency} </h2>
+          <h2>Enter Amount in {fromCurrency}</h2>
           <input type="number" autoComplete="off" value={fromAmount || ''} className="input" onChange={e => setFromAmount(e.target.value)} />
         </div>
         <div className="item3">
           <h3>=</h3>
-          <h2>Amount in   {toCurrency}</h2>
+          <h2>Amount in {toCurrency}</h2>
           <input className="output" disabled value={toAmount || ''} type="text" />
         </div>
         <div className="date">
@@ -141,11 +140,13 @@ function CurrencyConverter() {
           <input type="date" id="dateInput" value={selectedDate} onChange={handleDateChange} />
         </div>
         <div className="item4">
-          <h2>{fromAmount} {currencyNames[`${fromCurrency}`]} = {toAmount} {currencyNames[`${toCurrency}`]}</h2>
+          <h2>
+            {fromAmount} {currencyNames[`${fromCurrency}`]} = {toAmount} {currencyNames[`${toCurrency}`]}
+          </h2>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default CurrencyConverter
+export default CurrencyConverter;
